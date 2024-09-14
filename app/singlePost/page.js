a08896f39d10315dfc7ef6ef489f7ee4a3a71a3a
Umar Farooq <uf29664@gmail.com>
2024-09-14 17:56:11 +0500

    contentful

--- app/singlePost/page.js
+++ app/singlePost/page.js
@@ -21,15 +21,28 @@ return services
   
 }
 
+const fetchSecondData = async()=>{
+  let response = await client.getEntries({content_type:'singlePostSecond'})
+  const secondData = response.items.map((data)=>{
+    return{
+      heading:data.fields.heading,
+      pera:data.fields.pera,
+      firstPera:data.fields.secondPera
+    }
+  })
+  return secondData
+}
+
 
 export default async function page() {
   const singlePost = await fetchData()
-  console.log('singlePost',singlePost);
+  const secondData = await fetchSecondData()
+  console.log('singlePost',secondData);
   
   return (
     <div>
         <Navbar/>
-      <div className=" max-w-[800px] m-auto mt-7">
+      <div className=" max-w-[800px] m-auto mt-7 md:p-0 sm:p-5">
       <button className='text-md text-white mt-5 py-1 px-3 bg-blue-500 font-bold rounded-md'>Technology</button>
           <h1 className="text-[36px]     font-semibold pt-7">
             The Impact of Technology on the Workplace: <br /> How Technology is Changing
@@ -47,6 +60,17 @@ export default async function page() {
                 <div key={i}>
                   <p className="font-serif text-xl mt-5">{item.firstPera}</p>
                   <p className="font-serif text-xl mt-7">{item.secondPera}</p>
+                </div>
+              )
+            })
+          }
+          {
+            secondData.map((item,i)=>{
+              return(
+                <div key={i}>
+                  <h1 className="font-serif font-bold text-2xl mt-9">{item.heading}</h1>
+                  <p className="font-serif text-xl mt-5">{item.pera}</p>
+                  <p className="font-serif text-xl mt-7">{item.firstPera}</p>
                 </div>
               )
             })
